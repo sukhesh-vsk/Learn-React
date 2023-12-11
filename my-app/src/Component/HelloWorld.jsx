7773a8b748899b36c4add8ca1fedf22390ff069c
sukhesh-vsk <sukhesh.vsk2005@gmail.com>
2023-12-11 12:07:27 +0530

    Updated Named Export

--- my-app/src/Component/HelloWorld.jsx
+++ my-app/src/Component/HelloWorld.jsx
@@ -1,13 +1,10 @@
-// export default function Helloworld() {
-//     return (
-//         <h1>Hello, World!</h1>
-//     )
-// };
-export const Hello = () => {
+const Hello = () => {
     return (
         <h1>Hello, World!</h1>
     )
 };
 
+
+export { Hello };
 
 
